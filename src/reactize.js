a49f3e6ebd7a4e4f2c198de46c13c69b0160e139
Morgan Hallgren <morgan.hallgren@upptec.se>
2015-02-12 22:34:15 +0100

    remove turbolinks code

--- src/reactize.js
+++ src/reactize.js
@@ -1,5 +1,3 @@
-var turbolinks = require('./turbolinks.js');
-
 var diff = require('virtual-dom/diff');
 var patch = require('virtual-dom/patch');
 
@@ -22,17 +20,5 @@ Reactize.applyDiff = function(replacementElement, targetElement) {
   targetElement = patch(targetElement, patches);
   bodyVTree = replacementVtree;
 };
-
-function applyBodyDiff() {
-  //generate a globla virtual-dom tree for the body element
-  bodyVTree = convertHTML(document.body.outerHTML.trim());
-  global.removeEventListener("load", applyBodyDiff);
-}
-
-global.addEventListener("load", applyBodyDiff);
-
-// Turbolinks calls `replaceChild` on the document element when an update should
-// occur. Monkeypatch the method so Turbolinks can be used without modification.
-global.document.documentElement.replaceChild = Reactize.applyDiff;
 
 module.exports = global.Reactize = Reactize;
